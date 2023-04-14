bf23755feb1d83925c7dd323070bd67e8b015dbb
Aleksandr Baranov <a.n.baranov@outlook.com>
2023-04-14 23:00:59 +0300

    feat: logger added

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -4,11 +4,32 @@ import (
 	"log"
 
 	"github.com/Karzoug/loyalty_program/internal/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
+type buildLoggerConfig interface {
+	IsDebugMode() bool
+}
+
 func main() {
-	_, err := config.Read()
+	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("Read config error: %s", err)
 	}
+
+	logger, err := buildLogger(cfg)
+	if err != nil {
+		log.Fatalf("Create logger error: %s", err)
+	}
+	defer logger.Sync()
+}
+
+func buildLogger(cfg buildLoggerConfig) (*zap.Logger, error) {
+	if cfg.IsDebugMode() {
+		config := zap.NewDevelopmentConfig()
+		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		return config.Build()
+	}
+	return zap.NewProduction()
 }

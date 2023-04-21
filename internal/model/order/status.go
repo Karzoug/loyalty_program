2778d47451843876134c93d24341251c5b7f3a4c
Aleksandr Baranov <a.n.baranov@outlook.com>
2023-04-21 15:08:36 +0300

    refactor: order status simplified

--- internal/model/order/status.go
+++ internal/model/order/status.go
@@ -2,23 +2,13 @@ package order
 
 type status int8
 
-func (s status) String() string {
-	switch s {
-	case StatusNew:
-		return "NEW"
-	case StatusProcessing:
-		return "PROCESSING"
-	case StatusInvalid:
-		return "INVALID"
-	case StatusProcessed:
-		return "PROCESSED"
-	}
-	return ""
-}
-
 const (
 	StatusNew status = iota
 	StatusProcessing
 	StatusInvalid
 	StatusProcessed
 )
+
+func (s status) String() string {
+	return [...]string{"NEW", "PROCESSING", "INVALID", "PROCESSED"}[s]
+}
